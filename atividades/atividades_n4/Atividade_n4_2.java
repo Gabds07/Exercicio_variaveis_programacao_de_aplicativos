5cdc66f1458686e5241889cc4a91db71fa928ef9
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 16:01:40 -0300

    Add files via upload

---
+++ atividades/atividades_n4/Atividade_n4_2.java
@@ -0,0 +1,15 @@
+package atividades.atividade_n4;
+
+import java.util.Scanner;
+
+public class Atividade_n4_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        boolean value;
+        System.out.println("Digite um valor booleano: ");
+        value = scanner.nextBoolean();
+
+        System.out.println("Valor booleano digitado: " + value);
+    }
+}
