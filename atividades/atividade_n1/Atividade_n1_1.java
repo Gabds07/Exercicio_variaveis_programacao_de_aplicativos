b894fa8d72bfecceb9d98afaa4612524e42e2e8f
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 15:56:37 -0300

    Delete atividades directory

--- atividades/atividade_n1/Atividade_n1_1.java
+++
@@ -1,9 +0,0 @@
-package atividades.atividade_n1;
-
-public class Atividade_n1_1 {
-    public static void main() {
-        int valor = 3145;
-
-        System.out.println("O valor é: " + valor);
-    }    
-}
