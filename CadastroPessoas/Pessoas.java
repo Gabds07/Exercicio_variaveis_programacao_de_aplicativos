4fa55e559b45bd3eff8a891ef7c1829d9ea1d5aa
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-03-13 15:54:56 -0300

    Add Pessoa class with basic attributes and methods

---
+++ CadastroPessoas/Pessoas.java
@@ -0,0 +1,29 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + this.nome + ", idade: " + this.idade);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
